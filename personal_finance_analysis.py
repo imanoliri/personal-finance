b996d78b2005637677daf71b0790a6b542dd3679
Imanol <Imanol.Irizar13@gmail.com>
2024-01-28 09:56:40 +0100

    autoformatter

--- personal_finance_analysis.py
+++ personal_finance_analysis.py
@@ -1,20 +1,25 @@
-#%%
+# %%
 # # This notebook reads the input banking data and returns the personal finance stats by year
 
-#%% 
+# %%
 # Read the input data
 import pandas as pd
-banking_data_file = 'data/DKB_data_imanol.xlsx'
+
+banking_data_file = "data/DKB_data_imanol.xlsx"
 df = pd.read_excel(banking_data_file)
+df.dtypes
+# %%
+df.info()
+# %%
 df
-#%%
+# %%
 # Clean data should look like this:
 # -- Index: Year, Month, Day
 # -- Columns: Status (0,1), Sender, Receiver, Note, Type (in, out), IBAN, Ammount (€)
 # -- Delete columns: Believer-ID?, Reference, Customer Reference
-#%%
+# %%
 # Final personal finance report by year should look like this
 # -- Index: Year
 # -- Columns directly from data: (Cash start,) Cash end, Earned, Saved, Spent, Invested in X, Invested in Y, ...
 # -- Derived Columns: Investment Ratio, Investment in X, Investment in Y...
-#%%
+# %%
